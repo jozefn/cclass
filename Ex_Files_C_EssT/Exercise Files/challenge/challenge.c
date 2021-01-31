7783b05b71dbb28f6a143bbf7373f9484a2937b4
jozefn <jozefn999@gmail.com>
2021-01-31 15:55:12 -0800

    first commit of cclass

---
+++ Ex_Files_C_EssT/Exercise Files/challenge/challenge.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* generate mulitiple lines of output */
+
+int main()
+{
+
+    int rows = 0;
+
+    /* get and verify input */
+    printf(" enter a number of rows (18 max): ");
+    scanf("%d",&rows);
+
+    /* avoid out-of-range values */
+
+    /* process the rows */
+
+    printf(" the number rows: %d \n ",rows);
+
+    return 0;
+
+}
